8ccbc6b6e5666c7195df392295ae011050d6f85f
Ashish Bhutiani <ashish@function1.com>
2013-04-04 00:12:56 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BaseballPicker::Application.config.secret_token = '45d76c0224ef95f9efbcb0acd90d58aa66771fb68daaa408c61967b331d19e6a5df0124e7f9c0897568001dc853c0e27c548e8d6cc1c0336a53c953a29858d49'
